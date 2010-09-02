44831346f8a9b6c47d32e69d9c1276da41c37e3b
Jacek Becela <jacek.becela@gmail.com>
2010-09-02 15:19:03 +0200

    Slice path

--- lib/exvo_auth/dejavu.rb
+++ lib/exvo_auth/dejavu.rb
@@ -1,17 +1,18 @@
 class ExvoAuth::Dejavu
   def initialize(app)
-    @app = app
+    @app    = app
+    @prefix = "/auth/dejavu"
   end
   
   def call(env)
-    dejavu(env)
+    dejavu(env) if Rack::Request.new(env).path.start_with?(@prefix)
     @app.call(env)
   end
   
   private
   
   def dejavu(env)
-    data = MultiJson.decode(Base64.decode64(Rack::Request.new(env).params["stored_request"]))
+    data = MultiJson.decode(Base64.decode64(Rack::Request.new(env).path.slice(@prefix.size..-1)))
 
     env["QUERY_STRING"]   = Rack::Utiles.build_query(data[:params]) # Will not work with file uploads.
     env["SCRIPT_NAME"]    = data[:script_name]
