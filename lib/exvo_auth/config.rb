8f5f166fb3e9372618719796774ff7ef4ccc5f04
Jacek Becela <jacek.becela@gmail.com>
2010-10-06 12:47:45 +0200

    Tidy up configs

--- lib/exvo_auth/config.rb
+++ lib/exvo_auth/config.rb
@@ -15,29 +15,29 @@ module ExvoAuth::Config
     @@callback_key = callback_key 
   end
   
-  def self.client_id=(client_id)
-    @@client_id = client_id
-  end
-  
   def self.client_id
     @@client_id ||= nil
   end
   
-  def self.client_secret=(client_secret)
-    @@client_secret = client_secret
+  def self.client_id=(client_id)
+    @@client_id = client_id
   end
   
   def self.client_secret
     @@client_secret ||= nil 
   end
-  
-  # Set this to false during development ONLY!
-  def self.require_ssl=(require_ssl)
-    @@require_ssl = require_ssl
+
+  def self.client_secret=(client_secret)
+    @@client_secret = client_secret
   end
   
   def self.require_ssl
     @@require_ssl = true unless defined?(@@require_ssl)
     @@require_ssl
+  end
+
+  # Set this to false during development ONLY!
+  def self.require_ssl=(require_ssl)
+    @@require_ssl = require_ssl
   end
 end
