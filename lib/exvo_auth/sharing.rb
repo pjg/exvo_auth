2125d727d5ff427e055c6925a38bb15958a67454
Jacek Becela <jacek.becela@gmail.com>
2010-11-12 15:37:37 +0100

    Flesh out exists method

--- lib/exvo_auth/sharing.rb
+++ lib/exvo_auth/sharing.rb
@@ -2,9 +2,9 @@ class ExvoAuth::Sharing
   def self.create(attrs = {})
     new(attrs).save!
   end
-  
-  protected
 
+  protected
+  
   def initialize(attrs = {})
     raise ArgumentError, "Missing email or user_uid") unless attrs[:email] || attrs[:user_uid]
     raise ArgumentError, "Missing cfs_file_id")       unless attrs[:cfs_file_id]
@@ -17,15 +17,24 @@ class ExvoAuth::Sharing
     user_exists? ? send_email_to_existing_user : send_email_to_potential_user
   end
   
+  # Get user from auth by uid or email. Returns true if user exists.
+  def user_exists?
+    auth.get("...")
+  end
+
   def send_email_to_existing_user
     quick_download_url
     see_in_shared_items_url
+    # send
   end
   
   def send_email_to_potential_user
     quick_download_url
     save_to_shared_items_url
+    # send
   end
+  
+  private
   
   # Creates sharing in cfs and returns it.
   def sharing
@@ -51,16 +60,24 @@ class ExvoAuth::Sharing
   end
   
   def see_in_shared_items_url
-    attrs[:shared_items_url] + "?sharing_id=" + sharing["id"] # i.e. http://example.com/shared_items?sharing_id=123
+    append_url(attrs[:shared_items_url], :sharing_id => sharing["id"]) # i.e. http://example.com/shared_items?sharing_id=123
   end
   
   # Upon creating a sharing in cfs, user will be redirected to "return_to" url with one additional param: "sharing_id".
   # The app is then responsible to create a corresponding sharing locally.
   def save_to_shared_items_url
-    file["create_sharing_url"] + "?return_to?=" + attrs[:shared_items_url]
+    append_url(file["create_sharing_url"], :return_to => attrs[:shared_items_url])
   end
   
+  def append_url(url, params = {})
+    [url, RackUtils.build_query(params)].join(/\?/ === url ? "&" : "?")
+  end
+
   def cfs
-    @cfs ||= Consumer.new(:app_id => Config.cfs_id)
+    @cfs ||= Autonomous::Consumer.new(:app_id => Config.cfs_id)
+  end
+  
+  def auth
+    Autonomous::Auth.instance
   end
 end
