e415e76ed9b5c004b0d7231201dec5dbcc4e339e
Jacek Becela <jacek.becela@gmail.com>
2010-12-13 10:44:42 +0100

    Oauth2 state revelations

--- lib/exvo_auth/strategies/interactive.rb
+++ lib/exvo_auth/strategies/interactive.rb
@@ -5,7 +5,7 @@ class ExvoAuth::Strategies::Interactive < ExvoAuth::Strategies::Base
   
   def request_phase(options = {})
     super(unicorns_and_rainbows(
-      :scope     => request["scope"], 
+      :scope     => request["scope"],
       :state     => request["state"],
       :x_sign_up => request["x_sign_up"]
     ))
