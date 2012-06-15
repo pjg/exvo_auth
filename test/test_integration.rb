065b021572d8172140c79f1fa9bed2bc6e776f22
Paweł Gościcki <pawel.goscicki@gmail.com>
2012-06-15 21:17:53 +0200

    Fix integration test staging auth's uri

--- test/test_integration.rb
+++ test/test_integration.rb
@@ -2,13 +2,13 @@ require 'helper'
 
 class TestIntegration < Test::Unit::TestCase
   def setup
-    Exvo::Helpers.auth_host          = "staging.auth.exvo.com"
+    Exvo::Helpers.auth_host          = "auth.exvo.co"
     Exvo::Helpers.auth_client_id     = "foo"
     Exvo::Helpers.auth_client_secret = "bar"
     Exvo::Helpers.auth_require_ssl   = true
   end
 
-  test "integration with staging.auth.exvo.com" do
+  test "integration with auth.exvo.co" do
     c = ExvoAuth::Autonomous::Consumer.new(:app_id => "bar")
     authorization = c.send(:authorization)
     assert authorization["access_token"].size > 0
